c381ce6d13a01a9d1cb28262dc02a4b3a05fbb04
Chawks <chawkidrici9@gmail.com>
2026-08-09 20:30:54 +0200

    Update shop.js

--- shop.js
+++ shop.js
@@ -1,1 +1,45 @@
+document.addEventListener("DOMContentLoaded", () => {
 
+    const sliders = document.querySelectorAll(".image-slider");
+
+
+    sliders.forEach((slider) => {
+
+        const slides = slider.querySelectorAll(".slide");
+
+        const currentNumber =
+            slider.querySelector(".current");
+
+        let current = 0;
+
+
+        function changeSlide() {
+
+            slides[current].classList.remove("active");
+
+
+            current++;
+
+            if (current >= slides.length) {
+                current = 0;
+            }
+
+
+            slides[current].classList.add("active");
+
+
+            if (currentNumber) {
+
+                currentNumber.textContent =
+                    String(current + 1).padStart(2, "0");
+
+            }
+
+        }
+
+
+        setInterval(changeSlide, 3500);
+
+    });
+
+});
