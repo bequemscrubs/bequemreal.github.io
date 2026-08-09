c2a6e2315681b51ce481bcd564ca3bff29b438b3
Chawks <chawkidrici9@gmail.com>
2026-08-09 20:24:56 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,124 @@
+/* =========================
+   MOBILE MENU
+========================= */
 
+function toggleMenu() {
+    const menu = document.getElementById("mobileMenu");
+
+    menu.classList.toggle("active");
+}
+
+
+/* =========================
+   CART
+========================= */
+
+let cart = [];
+
+
+function openCart() {
+
+    const cartOverlay = document.getElementById("cartOverlay");
+
+    cartOverlay.classList.add("active");
+
+    displayCart();
+}
+
+
+function closeCart() {
+
+    const cartOverlay = document.getElementById("cartOverlay");
+
+    cartOverlay.classList.remove("active");
+}
+
+
+function addToCart(product) {
+
+    cart.push(product);
+
+    updateCartCount();
+
+    displayCart();
+
+}
+
+
+function updateCartCount() {
+
+    document.getElementById("cartCount").textContent = cart.length;
+
+}
+
+
+function displayCart() {
+
+    const cartItems = document.getElementById("cartItems");
+
+    if (cart.length === 0) {
+
+        cartItems.innerHTML = `
+            <div class="empty-cart">
+                Your cart is empty.
+            </div>
+        `;
+
+        return;
+    }
+
+
+    cartItems.innerHTML = "";
+
+    cart.forEach((item, index) => {
+
+        const product = document.createElement("div");
+
+        product.innerHTML = `
+            <div style="
+                padding:20px 0;
+                border-bottom:1px solid #111;
+            ">
+
+                <strong>${item.name}</strong>
+
+                <p style="
+                    margin-top:8px;
+                    font-size:12px;
+                ">
+                    ${item.price}
+                </p>
+
+                <button
+                    onclick="removeFromCart(${index})"
+                    style="
+                        margin-top:15px;
+                        border:none;
+                        background:none;
+                        text-decoration:underline;
+                        cursor:pointer;
+                        font-size:10px;
+                    "
+                >
+                    REMOVE
+                </button>
+
+            </div>
+        `;
+
+        cartItems.appendChild(product);
+
+    });
+
+}
+
+
+function removeFromCart(index) {
+
+    cart.splice(index, 1);
+
+    updateCartCount();
+
+    displayCart();
+
+}
